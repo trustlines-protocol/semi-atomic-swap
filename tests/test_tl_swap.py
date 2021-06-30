bbaec14f49929e6e2fe6691f0c7bee77a25a2e00
cducrest <come.ducrest@gmail.com>
2021-06-30 20:24:07 +0200

    Prevent commiting for someone and claiming for someone

--- tests/test_tl_swap.py
+++ tests/test_tl_swap.py
@@ -27,7 +27,7 @@ def sender(accounts):
 
 
 @pytest.fixture()
-def recipient(accounts):
+def receiver(accounts):
     return accounts[2]
 
 
@@ -38,11 +38,10 @@ def swap_tl_amount():
 
 @pytest.fixture()
 def commit_swap(
-    tl_swap_contract, tl_currency_network_contract, sender, recipient, swap_tl_amount
+    tl_swap_contract, tl_currency_network_contract, sender, receiver, swap_tl_amount
 ):
     tl_swap_contract.functions.commit(
-        sender,
-        recipient,
+        receiver,
         tl_currency_network_contract.address,
         swap_tl_amount,
         "0xBf6CA0E4b2B5C788dB424383A95fd019d2EB717f",
@@ -56,18 +55,11 @@ def test_tl_swap_emit_initiated_event(
     tl_swap_contract, tl_currency_network_contract, accounts, web3
 ):
     sender = accounts[1]
-    recipient = accounts[2]
+    receiver = accounts[2]
     network = tl_currency_network_contract.address
     amount = 100
     tl_swap_contract.functions.commit(
-        sender,
-        recipient,
-        network,
-        amount,
-        "0xBf6CA0E4b2B5C788dB424383A95fd019d2EB717f",
-        1,
-        WEEK_SECONDS,
-        HASHED_SECRET,
+        receiver, network, amount, receiver, 1, WEEK_SECONDS, HASHED_SECRET,
     ).transact({"from": sender})
 
     commit_initiated_event = get_single_event_of_contract(
@@ -76,7 +68,7 @@ def test_tl_swap_emit_initiated_event(
 
     assert commit_initiated_event.hash == HASHED_SECRET
     assert commit_initiated_event.sender == sender
-    assert commit_initiated_event.recipient == recipient
+    assert commit_initiated_event.receiver == receiver
     assert commit_initiated_event.TLNetwork == network
     assert commit_initiated_event.TLMoneyAmount == amount
     assert (
@@ -85,137 +77,127 @@ def test_tl_swap_emit_initiated_event(
     )
 
 
-def test_tl_swap_commit_entry_alredy_exists(tl_swap_contract):
+def test_tl_swap_commit_entry_alredy_exists(
+    tl_swap_contract, tl_currency_network_contract, sender, receiver
+):
 
     tl_swap_contract.functions.commit(
-        "0x9f9f3792287e87d84590E3fe3fD0B34B6E60531e",
-        "0x94934A417e05091319048701bd4cF61DC48962ac",
-        "0xa6C7310A1fc7A806Fd7c20B4b030501fCe2AC977",
+        receiver,
+        tl_currency_network_contract.address,
         100,
-        "0xBf6CA0E4b2B5C788dB424383A95fd019d2EB717f",
+        receiver,
         1,
         WEEK_SECONDS,
-        "f81b517a242b218999ec8eec0ea6e2ddbef2a367a14e93f4a32a39e260f686ad",
-    ).transact()
+        HASHED_SECRET,
+    ).transact({"from": sender})
 
     with pytest.raises(exceptions.TransactionFailed) as excinfo:
 
         tl_swap_contract.functions.commit(
-            "0x9f9f3792287e87d84590E3fe3fD0B34B6E60531e",
-            "0x94934A417e05091319048701bd4cF61DC48962ac",
-            "0xa6C7310A1fc7A806Fd7c20B4b030501fCe2AC977",
+            receiver,
+            tl_currency_network_contract.address,
             200,
-            "0xBf6CA0E4b2B5C788dB424383A95fd019d2EB717f",
+            receiver,
             1,
             WEEK_SECONDS,
-            "f81b517a242b218999ec8eec0ea6e2ddbef2a367a14e93f4a32a39e260f686ad",
-        ).transact()
+            HASHED_SECRET,
+        ).transact({"from": sender})
 
     assert "Entry already exists" in str(excinfo.value)
 
 
-def test_tl_swap_commit_tl_money_required(tl_swap_contract):
+def test_tl_swap_commit_tl_money_required(
+    tl_swap_contract, sender, receiver, tl_currency_network_contract
+):
     with pytest.raises(exceptions.TransactionFailed) as excinfo:
 
         tl_swap_contract.functions.commit(
-            "0x9f9f3792287e87d84590E3fe3fD0B34B6E60531e",
-            "0x94934A417e05091319048701bd4cF61DC48962ac",
-            "0xa6C7310A1fc7A806Fd7c20B4b030501fCe2AC977",
+            receiver,
+            tl_currency_network_contract.address,
             0,
             "0xBf6CA0E4b2B5C788dB424383A95fd019d2EB717f",
             1,
             WEEK_SECONDS,
             "f81b517a242b218999ec8eec0ea6e2ddbef2a367a14e93f4a32a39e260f686ad",
-        ).transact()
+        ).transact({"from": sender})
 
     assert "TL total money amount is required" in str(excinfo.value)
 
 
-def test_tl_swap_commit_eth_address_required(tl_swap_contract):
+def test_tl_swap_commit_eth_address_required(
+    tl_swap_contract, sender, receiver, tl_currency_network_contract
+):
     with pytest.raises(exceptions.TransactionFailed) as excinfo:
         tl_swap_contract.functions.commit(
-            "0x9f9f3792287e87d84590E3fe3fD0B34B6E60531e",
-            "0x94934A417e05091319048701bd4cF61DC48962ac",
-            "0xa6C7310A1fc7A806Fd7c20B4b030501fCe2AC977",
+            receiver,
+            tl_currency_network_contract.address,
             100,
             ZERO_ADDRESS,
             1,
             WEEK_SECONDS,
             "f81b517a242b218999ec8eec0ea6e2ddbef2a367a14e93f4a32a39e260f686ad",
-        ).transact()
+        ).transact({"from": sender})
 
     assert "Ethereum address is required" in str(excinfo.value)
 
 
-def test_tl_swap_commit_eth_amount_required(tl_swap_contract):
+def test_tl_swap_commit_eth_amount_required(
+    tl_swap_contract, sender, receiver, tl_currency_network_contract
+):
     with pytest.raises(exceptions.TransactionFailed) as excinfo:
         tl_swap_contract.functions.commit(
-            "0x9f9f3792287e87d84590E3fe3fD0B34B6E60531e",
-            "0x94934A417e05091319048701bd4cF61DC48962ac",
-            "0xa6C7310A1fc7A806Fd7c20B4b030501fCe2AC977",
+            receiver,
+            tl_currency_network_contract.address,
             100,
             "0xa6C7310A1fc7A806Fd7c20B4b030501fCe2AC977",
             0,
             WEEK_SECONDS,
             "f81b517a242b218999ec8eec0ea6e2ddbef2a367a14e93f4a32a39e260f686ad",
-        ).transact()
+        ).transact({"from": sender})
 
     assert "Eth total amount is required" in str(excinfo.value)
 
 
-def test_tl_swap_claim_tl_money(tl_swap_contract, tl_currency_network_contract):
+def test_tl_swap_claim_tl_money(
+    tl_swap_contract, tl_currency_network_contract, sender, receiver
+):
     tl_swap_contract.functions.commit(
-        "0x9f9f3792287e87d84590E3fe3fD0B34B6E60531e",
-        "0x94934A417e05091319048701bd4cF61DC48962ac",
+        receiver,
         tl_currency_network_contract.address,
         100,
         "0xBf6CA0E4b2B5C788dB424383A95fd019d2EB717f",
         1,
         WEEK_SECONDS,
         HASHED_SECRET,
-    ).transact()
-
-    commit_initiated_event = get_single_event_of_contract(
-        tl_swap_contract, "Commit", 0,
-    )["args"]
-
-    assert commit_initiated_event.hash == HASHED_SECRET
-    assert commit_initiated_event.TLMoneyAmount == 100
+    ).transact({"from": sender})
 
     extra_data = b""
     tl_swap_contract.functions.claim(
-        [
-            "0x9f9f3792287e87d84590E3fe3fD0B34B6E60531e",
-            "0x94934A417e05091319048701bd4cF61DC48962ac",
-        ],
-        MAX_FEE,
-        extra_data,
-        SECRET,
-    ).transact()
+        [sender, receiver], MAX_FEE, extra_data, SECRET,
+    ).transact({"from": receiver})
 
     currency_transfer_called = get_single_event_of_contract(
         tl_currency_network_contract, "Transfer", 0,
     )["args"]
 
-    assert (
-        currency_transfer_called._from == "0x9f9f3792287e87d84590E3fe3fD0B34B6E60531e"
-    )
-    assert currency_transfer_called._to == "0x94934A417e05091319048701bd4cF61DC48962ac"
+    assert currency_transfer_called._from == sender
+    assert currency_transfer_called._to == receiver
     assert currency_transfer_called._value == 100
     assert currency_transfer_called._extraData == extra_data
 
 
-def test_tl_swap_remove_commitment(tl_swap_contract):
+def test_tl_swap_remove_commitment(
+    tl_swap_contract, tl_currency_network_contract, sender, receiver
+):
     tl_swap_contract.functions.commit(
-        "0x9f9f3792287e87d84590E3fe3fD0B34B6E60531e",
-        "0x94934A417e05091319048701bd4cF61DC48962ac",
-        "0xa6C7310A1fc7A806Fd7c20B4b030501fCe2AC977",
+        receiver,
+        tl_currency_network_contract.address,
         100,
         "0xBf6CA0E4b2B5C788dB424383A95fd019d2EB717f",
         1,
         0,
         HASHED_SECRET,
-    ).transact()
+    ).transact({"from": sender})
 
     commit_initiated_event = get_single_event_of_contract(
         tl_swap_contract, "Commit", 0,
@@ -234,48 +216,24 @@ def test_tl_swap_remove_commitment(tl_swap_contract):
 
 
 @pytest.mark.usefixtures("commit_swap")
-def test_claim_removed_commitment(tl_swap_contract, chain, sender, recipient, web3):
+def test_claim_removed_commitment(tl_swap_contract, chain, sender, receiver, web3):
     expiry_time = web3.eth.getBlock("latest").timestamp + WEEK_SECONDS + 1
     chain.time_travel(expiry_time)
     tl_swap_contract.functions.removeCommitment(HASHED_SECRET).transact()
     with pytest.raises(exceptions.TransactionFailed):
         tl_swap_contract.functions.claim(
-            [sender, recipient], MAX_FEE, b"", SECRET,
+            [sender, receiver], MAX_FEE, b"", SECRET,
         ).transact()
 
 
-def test_commit_for_someone_else(
-    accounts, tl_currency_network_contract, tl_swap_contract
-):
-    committer = accounts[0]
-    sender = accounts[1]
-    recipient = accounts[2]
-    network = tl_currency_network_contract.address
-    amount = 100
-    with pytest.raises(exceptions.TransactionFailed):
-        tl_swap_contract.functions.commit(
-            sender,
-            recipient,
-            network,
-            amount,
-            "0xBf6CA0E4b2B5C788dB424383A95fd019d2EB717f",
-            1,
-            WEEK_SECONDS,
-            HASHED_SECRET,
-        ).transact({"from": committer})
-
-
 def test_claim_your_own_commitment(
-    accounts, tl_currency_network_contract, tl_swap_contract
+    accounts, tl_currency_network_contract, tl_swap_contract, sender, receiver
 ):
-    sender = accounts[1]
-    recipient = accounts[2]
     network = tl_currency_network_contract.address
     amount = 100
 
     tl_swap_contract.functions.commit(
-        sender,
-        recipient,
+        receiver,
         network,
         amount,
         "0xBf6CA0E4b2B5C788dB424383A95fd019d2EB717f",
@@ -288,4 +246,6 @@ def test_claim_your_own_commitment(
         sender_friend = accounts[3]
         path = [sender, sender_friend, sender]
 
-        tl_swap_contract.functions.claim(path, 0, b"", SECRET)
+        tl_swap_contract.functions.claim(path, 0, b"", SECRET).transact(
+            {"from": receiver}
+        )
